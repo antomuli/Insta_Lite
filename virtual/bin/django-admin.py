871d220e07c06c7d6bda22453a3450ee7e7d0620
Anthony Muli <mulianthony561@gmail.com>
2020-03-09 13:44:01 +0300

    Remove virtual from git

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/moringaschool/Documents/moringa-schools-projects/Insta_Lite/virtual/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
